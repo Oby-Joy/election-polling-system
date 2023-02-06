7d15f0a11b86521aa2dbb4cf068725b6a1409e20
Igbonekwu Joy <69874981+Oby-Joy@users.noreply.github.com>
2023-02-06 11:23:33 +0100

    Add files via upload

---
+++ migrations/2023_01_20_074305_create_candidates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('post_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('faculty');
+            $table->string('manifesto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
